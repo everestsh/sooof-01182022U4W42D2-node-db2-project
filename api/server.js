48090167ca59e39c51ab2aa48fe2239a7f78eb40
sooof <sooof@me.com>
2022-01-18 20:47:50 -0800

    api/server.js work

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
 const express = require("express")
-
+const carRouter = require('./cars/cars-router')
 const server = express()
 
 // DO YOUR MAGIC
+server.use(express.json())
+server.use('/api/car', carRouter)
 
 module.exports = server
