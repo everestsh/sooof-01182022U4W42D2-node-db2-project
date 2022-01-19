f46722112e4f96edf82c00be8c3cb6263744a89b
sooof <sooof@me.com>
2022-01-18 21:01:38 -0800

    TEST: /api/hello work

--- api/server.js
+++ api/server.js
@@ -6,4 +6,17 @@ const server = express()
 server.use(express.json())
 server.use('/api/car', carRouter)
 
+
+// http get :8000/hello
+server.use('/hello', (req, res)=>{
+    // res.send("Hello World!!!!")
+    res.status(200).json('hello world!!!!!!')
+})
+// http get :8000
+server.use('*', (req, res)=>{
+    res.status(404).json({
+        message: 'not fount'
+    })
+})
+
 module.exports = server
